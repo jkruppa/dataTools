06152f72cb903bfe386ab76748c0b6765b388beb
jkruppa <jochen.kruppa@gmail.com>
2016-12-13 12:48:08 +0100

    minor errors...

--- R/init.R
+++ R/init.R
@@ -8,17 +8,18 @@
 ##' @export
 init <- function(...) {
   require(pacman)
-  talk("Load demanded packages")
-
   par$packages <- switch(Sys.info()['sysname'],
                          "Windows" = {
                            grep("doMC|foreach", par$packages,
                                 invert = TRUE, value = TRUE)
                          })
+  talk("Load demanded packages: ", paste0(par$packages, collapse = " "))
   p_load(char = par$packages, character.only = TRUE)
   talk("Set max.print to ", par$max.print)
   options(max.print = par$max.print)
   talk("Register ", par$nCores, " CPU cores. Change with 'registerDoMC()'")  
   if(Sys.info()['sysname'] != "Windows")
     registerDoMC(par$nCores)
+  if(file.exists("init.R"))
+    source("init.R")
 }
